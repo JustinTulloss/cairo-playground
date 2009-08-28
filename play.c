2ca3dbb45e7c214820c46d6e138c21b5b9fb102d
Justin Tulloss <Justin.Tulloss@palm.com>
2009-08-28 01:03:11 -0700

    Changed it to an animation over a background picture

--- play.c
+++ play.c
@@ -27,6 +27,7 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 #include <cairo.h>
 #include <gtk/gtk.h>
@@ -34,29 +35,53 @@
 #define LEFT_BUTTON 1
 #define RIGHT_BUTTON 3
 
-static double coordx[100];
-static double coordy[100];
+static cairo_surface_t *image;
 
-static int count = 0;
+static gint xc = 85, yc = 85;
+static gint width = 0, height = 0;
+static gint xshift = 1, yshift = 1;
 
 static gboolean
 on_expose_event(GtkWidget *widget, GdkEventExpose *event, gpointer user_data)
 {
-    int i;
     cairo_t *cr;
     cr = gdk_cairo_create(widget->window);
 
-    cairo_set_source_rgb(cr, 0, 0, 0);
-    cairo_set_line_width(cr, 0.5);
+    /* Paint the background image first */
+    cairo_set_source_surface(cr, image, 0, 0);
+    cairo_paint(cr);
 
-    for (i = 0; i < count-1; i++) {
-        cairo_move_to(cr, coordx[i], coordy[i]);
-        cairo_line_to(cr, coordx[i+1], coordy[i+1]);
-    }
+    /* Now we have a clear context, draw our circle */
+    xc += xshift;
+    yc += yshift;
+    cairo_arc(cr, xc, yc, 10, 0, 2*M_PI);
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_set_line_width(cr, 4);
+    /* We now have an outline, but we aren't done with the arc since we want
+     * to fill it in. So, we preserve the stroke, then change things for the
+     * fill.
+     */
+    cairo_stroke_preserve(cr);
+    cairo_set_source_rgba(cr, .8, 0.2, 0.1, .7);
+    cairo_fill(cr);
 
-    count = 0;
-    cairo_stroke(cr);
+    /* Put our text under the bouncy ball */
+    cairo_set_source_rgba(cr, 0, 0, 0, 1);
+    cairo_select_font_face(cr, "Helvetica",
+            CAIRO_FONT_SLANT_NORMAL,
+            CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size(cr, 24);
+    cairo_move_to(cr, width - 400, 50);
+    cairo_show_text(cr, "keep your eye on the ball");
+
     cairo_destroy(cr);
+
+    if (yc <= 0 || yc >= height) {
+        yshift = -yshift;
+    }
+    if (xc <= 0 || xc >= width) {
+        xshift = -xshift;
+    }
 
     return FALSE;
 }
@@ -64,15 +89,15 @@ on_expose_event(GtkWidget *widget, GdkEventExpose *event, gpointer user_data)
 static gboolean
 clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data)
 {
-    if (event->button == LEFT_BUTTON) {
-        coordx[count] = event->x;
-        coordy[count++] = event->y;
-    }
+    return TRUE;
+}
 
-    if (event->button == RIGHT_BUTTON) {
-        gtk_widget_queue_draw(widget);
-    }
+static gboolean
+time_handler(GtkWidget *widget) {
+    if (widget->window == NULL)
+        return FALSE;
 
+    gtk_widget_queue_draw(widget);
     return TRUE;
 }
 
@@ -80,6 +105,10 @@ int
 main(int argc, char *argv[])
 {
     GtkWidget *window;
+
+    image = cairo_image_surface_create_from_png("bg.png");
+    width = cairo_image_surface_get_width(image);
+    height = cairo_image_surface_get_height(image);
 
     gtk_init(&argc, &argv);
 
@@ -96,14 +125,18 @@ main(int argc, char *argv[])
 
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_title(GTK_WINDOW(window), "Justin's KICKASS lines!");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+    gtk_window_set_default_size(GTK_WINDOW(window), width, height);
     gtk_widget_set_app_paintable(window, TRUE);
+
+    g_timeout_add(2, (GSourceFunc) time_handler, (gpointer) window);
 
     gtk_widget_show_all(window);
 
     printf("Starting with cairo version %s\n", cairo_version_string());
 
     gtk_main();
+
+    cairo_surface_destroy(image);
 
     return 0;
 }
